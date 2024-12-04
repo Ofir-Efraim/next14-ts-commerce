5aab673d35f89fe7071536ebb8c85b5b12ef442e
ofir <ofirefraim9@gmail.com>
2024-12-04 17:41:45 +0200

    change link

--- app/components/Payment/Payment.tsx
+++ app/components/Payment/Payment.tsx
@@ -42,7 +42,7 @@ export default function Payment({ order }: paymentProps) {
             rel="noopener noreferrer"
             target="_blank"
             className={`${styles.paymentButton} ${styles.paybox}`}
-            href="https://payboxapp.page.link/BCFJipPAWaTuWBFo7"
+            href="https://payboxapp.page.link/dAZK3xwRh1cBgQzE7"
           >
             <Image width={54} height={54} src={paybox} alt="paybox" />
           </a>
